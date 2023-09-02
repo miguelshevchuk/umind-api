a59ccc5d02f8dbe82dff7e837f5fbc76dba5e8fc
Migue <shevchuk.miguel@gmail.com>
2023-09-01 21:02:09 -0300

    0000

--- app.py
+++ app.py
@@ -25,6 +25,5 @@ def getResult():
     return jsonify(results)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
-    serve(app, port=5000)
+app.run(host="0.0.0.0")
+serve(app, port=5000)
